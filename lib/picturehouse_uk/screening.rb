bb452e1ef6c88451d03c1690048b89d51be39f4e
Andy Croll <andy@goodscary.com>
2014-10-15 23:10:00 +0100

    rebuild Screening
    include .at
    use parser
    takes options hash
    test with MockWebsite

--- lib/picturehouse_uk/screening.rb
+++ lib/picturehouse_uk/screening.rb
@@ -1,36 +1,77 @@
 module PicturehouseUk
-
   # A single screening of a film on the Picturehouse UK website
   class Screening
-
+    # @return [String] the booking URL on the cinema website
+    attr_reader :booking_url
     # @return [String] the cinema name
     attr_reader :cinema_name
+    # @return [String] 2d or 3d
+    attr_reader :dimension
     # @return [String] the film name
     attr_reader :film_name
-    # @return [Time] the UTC time of the screening
-    attr_reader :when
-    # @return [String] the type of screening (2D, 3D, IMAX...)
-    attr_reader :variant
 
-    # @param [String] film_name the film name
-    # @param [String] cinema_name the cinema name
-    # @param [Time] time datetime of the screening (UTC preferred)
-    # @param [String] variant the type of showing (e.g. 3d/baby/live)
-    def initialize(film_name, cinema_name, time, variant=nil)
-      @cinema_name, @film_name, @variant = cinema_name, film_name, variant
-      @when = time.utc? ? time : TZInfo::Timezone.get('Europe/London').local_to_utc(time)
+    # @param [Hash] options
+    def initialize(options)
+      @booking_url = options.fetch(:booking_url, nil)
+      @cinema_name = options.fetch(:cinema_name)
+      @cinema_id   = options.fetch(:cinema_id)
+      @dimension   = options.fetch(:dimension, '2d')
+      @film_name   = options.fetch(:film_name)
+      @time        = options.fetch(:time)
+      @variant     = options.fetch(:variant, [])
+    end
+
+    def self.at(cinema_id)
+      cinema_page(cinema_id).film_html.map do |html|
+        create_for_single_film(html, cinema_id)
+      end.flatten
+    end
+
+    # The UTC time of the screening
+    # @return [Time]
+    def showing_at
+      @showing_at ||= begin
+        if @time.utc?
+          @time
+        else
+          TZInfo::Timezone.get('Europe/London').local_to_utc(@time)
+        end
+      end
     end
 
     # The date of the screening
     # @return [Date]
-    def date
-      @when.to_date
+    def showing_on
+      showing_at.to_date
     end
 
-    # @deprecated Please use {#variant} instead, I can't spell
-    def varient
-      warn "Please use #variant instead, I can't spell"
-      variant
+    # The kinds of screening
+    # @return <Array[String]>
+    def variant
+      @variant.map(&:downcase).sort
+    end
+
+    private
+
+    def self.cinema_hash(cinema_id)
+      {
+        cinema_id: cinema_id,
+        cinema_name: PicturehouseUk::Cinema.find(cinema_id).name
+      }
+    end
+
+    def self.cinema_page(cinema_id)
+      PicturehouseUk::Internal::CinemaPage.new(cinema_id)
+    end
+
+    def self.create_for_single_film(html, cinema_id)
+      screenings_parser(html).to_a.map do |attributes|
+        new cinema_hash(cinema_id).merge(attributes)
+      end
+    end
+
+    def self.screenings_parser(html)
+      PicturehouseUk::Internal::FilmWithScreeningsParser.new(html)
     end
   end
 end
--- test/lib/picturehouse_uk/screening_test.rb
+++ test/lib/picturehouse_uk/screening_test.rb
@@ -1,41 +1,169 @@
 require_relative '../../test_helper'
 
 describe PicturehouseUk::Screening do
+  let(:website) { MockWebsite.new }
 
   before { WebMock.disable_net_connect! }
 
-  describe '#new film_name, cinema_name, date, time, variant' do
-    it 'stores film_name, cinema_name & when (in UTC)' do
-      screening = PicturehouseUk::Screening.new 'Iron Man 3', "Duke's At Komedia", Time.parse('2013-09-12 11:00')
-      screening.film_name.must_equal 'Iron Man 3'
-      screening.cinema_name.must_equal "Duke's At Komedia"
-      screening.when.must_equal Time.utc(2013, 9, 12, 10, 0)
-      screening.variant.must_equal nil
+  describe '.at(cinema_id)' do
+    subject { PicturehouseUk::Screening.at('Duke_Of_Yorks') }
+
+    it 'returns an array of screenings' do
+      PicturehouseUk::Internal::Website.stub :new, website do
+        subject.must_be_instance_of(Array)
+        subject.each do |screening|
+          screening.must_be_instance_of(PicturehouseUk::Screening)
+        end
+      end
     end
 
-    it 'stores variant if passed' do
-      screening = PicturehouseUk::Screening.new 'Iron Man 3', "Duke's At Komedia", Time.utc(2013, 9, 12, 11, 0), 'baby'
-      screening.film_name.must_equal 'Iron Man 3'
-      screening.cinema_name.must_equal "Duke's At Komedia"
-      screening.when.must_equal Time.utc(2013, 9, 12, 11, 0)
-      screening.variant.must_equal 'baby'
+    it 'returns correct number of screenings' do
+      PicturehouseUk::Internal::Website.stub :new, website do
+        subject.count.must_equal 29
+      end
     end
   end
 
-  describe '#date' do
-    subject { PicturehouseUk::Screening.new('Iron Man 3', "Duke's At Komedia", Time.utc(2013, 9, 12, 11, 0), '3d').date }
-    it 'should return date of showing' do
+  describe '.new' do
+    subject { PicturehouseUk::Screening.new(options) }
+
+    describe 'simple' do
+      let(:options) do
+        {
+          film_name:   'Iron Man 3',
+          cinema_id:   3,
+          cinema_name: 'Cineworld Brighton',
+          time:        Time.utc(2013, 9, 12, 11, 0)
+        }
+      end
+
+      it 'sets cinema name and film name' do
+        subject.film_name.must_equal 'Iron Man 3'
+        subject.cinema_name.must_equal 'Cineworld Brighton'
+      end
+
+      it 'booking url, dimension & varient are set to defaults' do
+        subject.booking_url.must_equal nil
+        subject.dimension.must_equal '2d'
+        subject.variant.must_equal []
+      end
+    end
+  end
+
+  describe '#dimension' do
+    let(:options) do
+      {
+        film_name:   'Iron Man 3',
+        dimension:   '3d',
+        cinema_id:   3,
+        cinema_name: 'Cineworld Brighton',
+        time:        Time.utc(2013, 9, 12, 11, 0)
+      }
+    end
+
+    subject { PicturehouseUk::Screening.new(options).dimension }
+
+    it 'returns 2d or 3d' do
+      subject.must_be_instance_of(String)
+      subject.must_equal '3d'
+    end
+  end
+
+  describe '#showing_at' do
+    subject { PicturehouseUk::Screening.new(options).showing_at }
+
+    describe 'with utc time' do
+      let(:options) do
+        {
+          film_name:   'Iron Man 3',
+          cinema_id:   3,
+          cinema_name: 'Cineworld Brighton',
+          time:        Time.utc(2013, 9, 12, 11, 0)
+        }
+      end
+
+      it 'returns UTC time' do
+        subject.must_be_instance_of Time
+        subject.must_equal Time.utc(2013, 9, 12, 11, 0)
+      end
+    end
+
+    describe 'with non-utc time' do
+      let(:options) do
+        {
+          film_name:   'Iron Man 3',
+          cinema_id:   3,
+          cinema_name: 'Cineworld Brighton',
+          time:        Time.parse('2013-09-12 11:00')
+        }
+      end
+
+      it 'returns UTC time' do
+        subject.must_be_instance_of Time
+        subject.must_equal Time.utc(2013, 9, 12, 10, 0)
+      end
+    end
+  end
+
+  describe '#showing_on' do
+    let(:options) do
+      {
+        film_name:   'Iron Man 3',
+        cinema_id:   3,
+        cinema_name: 'Cineworld Brighton',
+        time:        Time.utc(2013, 9, 12, 11, 0)
+      }
+    end
+
+    subject { PicturehouseUk::Screening.new(options).showing_on }
+
+    it 'returns date of showing' do
       subject.must_be_instance_of(Date)
       subject.must_equal Date.new(2013, 9, 12)
     end
   end
 
-  describe '#varient (DEPRECATED)' do
-    subject { screening.varient }
-    let(:screening) { PicturehouseUk::Screening.new('Iron Man 3', "Duke's At Komedia", Time.utc(2013, 9, 12, 11, 0), '3d') }
+  describe '#variant' do
+    subject { PicturehouseUk::Screening.new(options).variant }
 
-    it 'should return variant' do
-      subject.must_equal '3d'
+    let(:options) do
+      {
+        film_name:   'Iron Man 3',
+        cinema_id:   3,
+        cinema_name: 'Cineworld Brighton',
+        time:        Time.utc(2013, 9, 12, 11, 0),
+        variant:     ['Kids']
+      }
+    end
+
+    it 'is an alphabetically ordered array of lower-cased strings' do
+      subject.must_be_instance_of Array
+      subject.each do |tag|
+        tag.must_be_instance_of String
+      end
+      subject.must_equal %w(kids)
+    end
+  end
+
+  private
+
+  class MockWebsite
+    def home
+      read_file('../../../fixtures/home.html')
+    end
+
+    def cinema(filename)
+      read_file("../../../fixtures/cinema/#{filename}.html")
+    end
+
+    def contact_us(filename)
+      read_file("../../../fixtures/contact_us/#{filename}.html")
+    end
+
+    private
+
+    def read_file(filepath)
+      File.read(File.expand_path(filepath, __FILE__))
     end
   end
 end
